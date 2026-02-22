f4fac20ed2ad7907febbb3e52bf9b08370623642
Guilherme <guilherme.luccas@outlook.com>
2026-02-22 18:50:32 -0300

    Enable resource shrinking and minification for release builds; add new minifiedDebug build type with application ID and version name suffixes for testing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,11 +21,23 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        create("minifiedDebug") {
+            initWith(getByName("debug"))
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            applicationIdSuffix = ".minified"
+            versionNameSuffix = "-minified"
         }
     }
     compileOptions {
